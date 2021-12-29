34b378c0b64b5656291396604bd9463f67e01248
Brian Brookman <bcbrookman@users.noreply.github.com>
2021-12-29 13:47:09 -0800

    Add Python script to remove unnecessary lines from conf files

---
+++ clean_conf_files.py
@@ -0,0 +1,44 @@
+# This helper script cleans up Wireshark profile config files by removing commented, blank, and unnecessary lines.
+
+import os
+import re
+
+
+def is_filtered(line_text):
+    filter_rules = [
+        {
+            "condition": re.search("^#.+file for Wireshark \\d+\\.\\d+\\.\\d+", line_text),
+            "filtered": False
+        },
+        {
+            "condition": line_text in ['\n', '\r\n'],
+            "filtered": True
+        },
+        {
+            "condition": line_text.startswith("#"),
+            "filtered": True
+        },
+    ]
+
+    for rule in filter_rules:
+        if rule["condition"]:
+            return rule["filtered"]
+
+
+if __name__ == "__main__":
+    for root, dirs, files in os.walk("."):  # Finds all files under the current working directory
+        if root.__contains__("bcb_"):   # Only change files where the root path contains "bcb_"
+            for conf_file in files:
+
+                # Read the current file, and exclude any lines matching is_filtered() filter rules
+                with open(os.path.join(root, conf_file), "r") as current_file:
+                    unfiltered_lines = []
+                    for line in current_file:
+                        if is_filtered(line):
+                            continue
+                        else:
+                            unfiltered_lines.append(line)
+
+                # Open the current file again, replacing its contents with only the remaining unfiltered lines
+                with open(os.path.join(root, conf_file), "w") as current_file:
+                    current_file.writelines(unfiltered_lines)
